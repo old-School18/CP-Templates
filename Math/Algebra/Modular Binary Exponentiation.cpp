2c836afa2e664397dafe22e973ad88b5585815f9
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2025-10-23 04:13:06 +0530

    Add modular binary exponentiation functions
    Implemented modular binary exponentiation using both recursive and iterative methods.

---
+++ Math/Algebra/Modular Binary Exponentiation.cpp
@@ -0,0 +1,27 @@
+// Time Complexity: O(log y)
+// (x^y) % M
+
+// Recursive
+ll mod_exponent(ll x, ll y, ll M)
+{
+    if (!y)
+        return 1ll;
+    else if (!(y % 2ll))
+        return mod(mod_exponent(mod(x * x, M), y / 2ll, M), M);
+    else
+        return mod(x * mod_exponent(mod(x * x, M), y / 2ll, M), M);
+}
+
+// Looping
+ll mod_exponent_loop(ll x, ll y, ll M)
+{
+    ll result = 1ll;
+    while (y)
+    {
+        if (y & 1ll)
+            result = mod(result * x, M);
+        x = mod(x * x, M);
+        y >>= 1ll;
+    }
+    return result;
+}
