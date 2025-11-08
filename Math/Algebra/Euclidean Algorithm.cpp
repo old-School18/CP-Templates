eba165d2b9a94e3940ae6d4ecf37620c547cbb1d
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2025-11-08 15:55:47 +0530

    Add Euclidean Algorithm implementation in C++
    Both recursive & looping

---
+++ Math/Algebra/Euclidean Algorithm.cpp
@@ -0,0 +1,27 @@
+// Time Complexity: O(log(min(a,b)))
+// gcd(a,b)
+
+ll mod(ll a, ll M)
+{
+    ll absoluteM = abs(M);
+    return (a % absoluteM + absoluteM) % absoluteM;
+}
+
+// Recursive
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, mod(a, b));
+}
+
+// Looping
+ll gcd(ll a, ll b)
+{
+    while (b)
+    {
+        a = mod(a, b);
+        swap(a, b);
+    }
+    return a;
+}
