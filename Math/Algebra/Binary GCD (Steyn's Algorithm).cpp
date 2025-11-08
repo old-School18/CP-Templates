f485ddd29bd1c090260ef057838603cfb299e457
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2025-11-08 16:44:35 +0530

    Implement Binary GCD using Steyn's Algorithm
    Binary GCD

---
+++ Math/Algebra/Binary GCD (Steyn's Algorithm).cpp
@@ -0,0 +1,43 @@
+// Time Complexity: O(log(min(a,b)))
+// Faster than Euclidean as shift operations are cheaper than modulo
+// gcd(a,b)
+
+// Recursive
+ll gcd_recursive(ll a, ll b)
+{
+    if (!a)
+        return b;
+    a >>= __builtin_ctz(a);
+    if (a < b)
+        swap(a, b);
+    a -= b;
+    return gcd_recursive(a, b);
+}
+
+ll gcd(ll a, ll b)
+{
+    if (!a || !b)
+        return a | b;
+
+    ll common_even_divisor = __builtin_ctz(a | b);
+    b >>= __builtin_ctz(b);
+
+    return gcd_recursive(a, b) << common_even_divisor;
+}
+
+// Looping
+ll gcd(ll a, ll b)
+{
+    if (!a || !b)
+        return a | b;
+    ll common_even_divisor = __builtin_ctz(a | b);
+    b >>= __builtin_ctz(b);
+    while (a)
+    {
+        a >>= __builtin_ctz(a);
+        if (a < b)
+            swap(a, b);
+        a -= b;
+    }
+    return b << common_even_divisor;
+}
