6262ec8f70de12a7b65e48fabb09edf703c49f88
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2025-11-10 03:42:20 +0530

    Add Fibonacci number computation in C++
    Implement Fibonacci number computation using matrix exponentiation.

---
+++ Math/Algebra/Fibonacci.cpp
@@ -0,0 +1,42 @@
+// Time Complexity: O(log n)
+// Fibonnaci numbers computation
+
+struct matrix
+{
+    ll mat[2][2];
+
+    matrix friend operator*(const matrix &a, const matrix &b)
+    {
+        matrix c;
+        for (ll i = 0; i < 2; i++)
+        {
+            for (ll j = 0; j < 2; j++)
+            {
+                c.mat[i][j] = 0;
+                for (ll k = 0; k < 2; k++)
+                {
+                    c.mat[i][j] += a.mat[i][k] * b.mat[k][j];
+                }
+            }
+        }
+        return c;
+    }
+};
+
+ll fibonacci(ll n)
+{
+    matrix base{{{1, 1}, {1, 0}}};
+
+    matrix ans{{{1, 0}, {0, 1}}};
+
+    while (n)
+    {
+        if (n & 1)
+        {
+            ans = ans * base;
+        }
+        base = base * base;
+        n >>= 1;
+    }
+    return ans.mat[0][1];
+}
