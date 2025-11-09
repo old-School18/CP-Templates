1df11617ece8d336d4b0b52ba58b2d33ac1f3958
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2025-11-09 22:29:27 +0530

    Add Linear Diophantine Equation solver
    Implement Linear Diophantine Equation solver with extended Euclidean algorithm.

---
+++ Math/Algebra/Linear Diophantine Equation.cpp
@@ -0,0 +1,68 @@
+// Time Complexity: O(log(min(a,b)))
+// Any one (x,y) that satisfy ax+by=c
+
+ll mod(ll a, ll M)
+{
+    ll absoluteM = abs(M);
+
+    return (a % absoluteM + absoluteM) % absoluteM;
+}
+
+ll extended_euclid(ll a, ll b, ll &x, ll &y)
+{
+    if (b == 0)
+    {
+        x = 1, y = 0;
+        return a;
+    }
+
+    ll g = extended_euclid(b, mod(a, b), x, y);
+    tie(x, y) = make_tuple(y, x - (a / b) * y);
+    return g;
+}
+
+bool find_any_solution(ll a, ll b, ll c, ll &x, ll &y)
+{
+    if (a == 0 && b == 0 && c == 0)
+    {
+        return true; // infinite solutions
+    }
+    else if (a == 0 && b == 0)
+    {
+        return false;
+    }
+    ll g = extended_euclid(abs(a), abs(b), x, y);
+    if (c % g)
+    {
+        return false;
+    }
+    x *= (c / g);
+    y *= (c / g);
+
+    if (a < 0)
+        x = -x;
+    if (b < 0)
+        y = -y;
+    return true;
+}
+
+Derivation:
+
+ax+by=c
+
+If a and b are not co-prime, we should be able to extract the gcd(a,b) from both the terms on the left side of the equation i.e.,
+
+g*((a/g)x + (b/g)y) = c where g= gcd(a,b)
+
+This implies that c must also be divisible by g for the equation to have a solution.
+
+We know that Extended Euclid algorithm gives: ax+by=g
+
+So,
+ax1+by1=g
+
+ax1.(c/g)+by1.(c/g) = c
+
+which means, x=x1*(c/g) and y=y1*(c/g)
+
+So just evaluate extended euclidean which gives x1 and y1 and multiply by (c/g) to get a solution for ax+by=c.
