0ebf2faa6cb7ad80fbaaf9ccca7dbe41278104f4
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2025-11-15 12:39:51 +0530

    Add Linear Sieve algorithm implementation
    Implement linear sieve algorithm to find prime numbers.

---
+++ Math/Algebra/Linear Sieve.cpp
@@ -0,0 +1,23 @@
+// Time Complexity: O(n)
+// Memory Complexity: O(n)
+// Determine prime numbers till n in linear time
+
+void linear_sieve(ll n, vector<ll> &primes)
+{
+    vector<ll> lp(n + 1);
+
+    for (ll i = 2; i <= n; i++)
+    {
+        if (lp[i] == 0)
+        {
+            lp[i] = i;
+            primes.pb(i);
+        }
+        for (ll j = 0; primes[j] * i <= n && primes[j] <= lp[i]; j++)
+        {
+            lp[primes[j] * i] = primes[j];
+        }
+    }
+
+    return;
+}
