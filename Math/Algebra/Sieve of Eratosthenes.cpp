9aeceb095fc54ff0bcffdabc2345580e93fca1fc
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2025-11-15 13:57:11 +0530

    Update Sieve of Eratosthenes.cpp

--- Math/Algebra/Sieve of Eratosthenes.cpp
+++ Math/Algebra/Sieve of Eratosthenes.cpp
@@ -1,17 +1,18 @@
-// Time Complexity: O(n log log n)
+// Time Complexity => O(n log log n)
+// Memory Complexity => O(n)
 // Determine prime numbers from 1 to n.
 
 void sieve_of_eratosthenes(ll n)
 {
-    vector<bool> is_prime(n + 1, true);
-    is_prime[0] = is_prime[1] = false;
+    vector<char> is_prime(n + 1, 1);
+    is_prime[0] = is_prime[1] = 0;
     for (ll i = 2; i * i <= n; i++)
     {
         if (is_prime[i])
         {
             for (ll j = i * i; j <= n; j += i)
             {
-                is_prime[j] = false;
+                is_prime[j] = 0;
             }
         }
     }
@@ -20,4 +21,4 @@ void sieve_of_eratosthenes(ll n)
 }
 
 -----------------------------------
-Note:  vector<bool> operates at bit level rather than bytes so 1 element will occupy only 1 bit instead of a byte that a typical boolean variables does. So, this is memory efficient
+Note:  Use vector<char> for time efficiency and vector<bool> for memory efficiency. vector<bool> operates at bit level rather than bytes so 1 element will occupy only 1 bit instead of a byte that a typical boolean variables does. So, this is memory efficient
