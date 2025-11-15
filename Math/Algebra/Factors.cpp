8e640186ae20c9cc2e043d87f69a70041dafcd20
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2025-11-15 14:58:56 +0530

    Update Factors.cpp

--- Math/Algebra/Factors.cpp
+++ Math/Algebra/Factors.cpp
@@ -2,17 +2,16 @@
 // Memory Complexity => O(n)
 // Integer factors of a number
 
-vector<ll> factors(ll n)
+void factors(ll n, vector<ll> &factors)
 {
-    vector<ll> v;
     for (ll i = 1; i * i <= n; i++)
     {
         if (n % i == 0)
         {
-            v.push_back(i);
+            factors.push_back(i);
             if (n / i != i)
-                v.push_back(n / i);
+                factors.push_back(n / i);
         }
     }
-    return v;
+    return;
 }
