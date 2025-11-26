ee23c6b229e91b9978174211e78d90785a88f8a1
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2025-11-26 15:13:52 +0530

    Add Modulus Cyclic.cpp file

--- Math/Algebra/Modulus Cyclic.cpp
+++ Math/Algebra/Modulus Cyclic.cpp
@@ -2,7 +2,7 @@
 // Memory Complexity => O(1)
 // Resultant value is always between 0 and |M|-1 even when divisor or dividend is negative
 
-ll mod(ll a, ll M)
+ll mod_cyclic(ll a, ll M)
 {
     ll absoluteM = abs(M);
     return (a % absoluteM + absoluteM) % absoluteM;
