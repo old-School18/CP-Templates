6d57c7d892031b7161809bcd22b80eeeb0ee5e1c
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2025-11-26 15:10:46 +0530

    Refactor modular inverse calculation

--- Math/Algebra/Modular Inverse.cpp
+++ Math/Algebra/Modular Inverse.cpp
@@ -2,12 +2,6 @@
 // Memory Complexity => Recursive: O(log min(a,M)) | Iterative: O(1)  [Extended Eulid]
 // Determines 'inv' where (a * inv) % M = 1
 
-ll mod(ll a, ll M)
-{
-    ll absoluteM = abs(M);
-    return (a % absoluteM + absoluteM) % absoluteM;
-}
-
 ll extended_euclid(ll a, ll b, ll &x, ll &y)
 {
     if (b == 0)
@@ -15,7 +9,7 @@ ll extended_euclid(ll a, ll b, ll &x, ll &y)
         x = 1, y = 0;
         return a;
     }
-    ll g = extended_euclid(b, mod(a, b), x, y);
+    ll g = extended_euclid(b, a%b, x, y);
     tie(x, y) = make_tuple(y, x - (a / b) * y);
     return g;
 }
@@ -27,5 +21,5 @@ ll mod_inverse(ll a, ll M)
     if (g != 1) {
     	return -1; // No inverse
     }
-    return mod(x, M);
+    return x%M;
 }
