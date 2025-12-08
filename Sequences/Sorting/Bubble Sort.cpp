50c5a0751461612889bed2b4a8795499aed3aaa7
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2025-12-08 18:22:35 +0530

    Add Bubble Sort implementation in C++
    Implemented the Bubble Sort algorithm in C++ with time and memory complexity annotations.

---
+++ Sequences/Sorting/Bubble Sort.cpp
@@ -0,0 +1,21 @@
+// Time Complexity: O(n2) Worst | O(n2) Average | O(n) Best
+// Memory Complexity: O(1)
+// Stable: Yes
+// Mechanism to sort by moving the next largest element to the last unsorted index
+
+void bubble_sort(ll a[], ll n)
+{
+    bool swapped=true;
+    
+    for(ll i=n-1;i>=0&&swapped;i--) {
+    	swapped=false;
+    	for(ll j=0;j<i;j++) {
+    		if(a[j]>a[j+1]) {
+    			swapped=true;
+    			swap(a[j],a[j+1]);
+    		}
+    	}
+    }
+    
+    return;
+}
