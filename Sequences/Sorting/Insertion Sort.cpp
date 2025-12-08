ebc81d8d1b7906f2342bb16cc7fdfa93a5d3e7ed
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2025-12-08 18:51:52 +0530

    Add Insertion Sort implementation in C++
    Implemented insertion sort algorithm for sorting sequences.

---
+++ Sequences/Sorting/Insertion Sort.cpp
@@ -0,0 +1,17 @@
+// Time Complexity: O(n2) Worst | O(n2) Average | O(n) Best
+// Memory Complexity: O(1)
+// Stable: Yes
+// Mechanism to sort sequences by INSERTING the current iterating index into its appropriate position
+
+void insertion_sort(ll a[],ll n) {
+	for(ll i=1;i<n;i++) {
+		ll curr=a[i];
+		ll j=i-1;
+		while(a[j]>curr&&j>=0) {
+			a[j+1]=a[j];
+			j--;
+		}
+		a[j+1]=curr;
+	}
+	return;
+}
