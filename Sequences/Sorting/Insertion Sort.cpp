2f1d781649b945216162ae78bda632e4335562d8
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2025-12-09 21:45:41 +0530

    Format insertion sort function for consistency

--- Sequences/Sorting/Insertion Sort.cpp
+++ Sequences/Sorting/Insertion Sort.cpp
@@ -4,14 +4,16 @@
 // Mechanism to sort sequences by INSERTING the current iterating index into its appropriate position
 
 void insertion_sort(ll a[],ll n) {
-	for(ll i=1;i<n;i++) {
-		ll curr=a[i];
-		ll j=i-1;
-		while(a[j]>curr&&j>=0) {
-			a[j+1]=a[j];
-			j--;
-		}
-		a[j+1]=curr;
-	}
-	return;
+    for (ll i = 1; i < n; i++)
+    {
+        ll curr = a[i];
+        ll j = i - 1;
+        while (a[j] > curr && j >= 0)
+        {
+            a[j + 1] = a[j];
+            j--;
+        }
+        a[j + 1] = curr;
+    }
+    return;
 }
