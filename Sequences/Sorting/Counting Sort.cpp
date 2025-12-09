56c6322676987c15b07b90825e8358d6eac724e3
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2025-12-09 21:44:05 +0530

    Add Counting Sort implementation in C++
    Implemented Counting Sort algorithm with time and memory complexity details.

---
+++ Sequences/Sorting/Counting Sort.cpp
@@ -0,0 +1,37 @@
+// Time Complexity: O(n+m) where m — maximum element in the sequence
+// Memory Complexity: O(n+m)
+// Stable: Yes
+// Mechanism to sort sequences by COUNTING number of elements less than equal to each element in the sequence
+
+void counting_sort(ll a[], ll n)
+{
+    ll max_elem = a[0];
+    for (ll i = 0; i < n; i++)
+    {
+        max_elem = max(max_elem, a[i]);
+    }
+
+    ll count[max_elem + 1] = {0};
+
+    for (ll i = 0; i < n; i++)
+    {
+        count[a[i]]++;
+    }
+    for (ll i = 1; i <= max_elem; i++)
+    {
+        count[i] += count[i - 1];
+    }
+
+    ll temp[n];
+    for (ll i = n - 1; i >= 0; i--)
+    {
+        temp[count[a[i]] - 1] = a[i];
+        count[a[i]]--;
+    }
+
+    for (ll i = 0; i < n; i++)
+    {
+        a[i] = temp[i];
+    }
+    return;
+}
