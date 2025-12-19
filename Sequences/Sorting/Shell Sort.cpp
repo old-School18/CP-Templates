3243f992246bb9cd4f07a39c1f16d3dd7386a7de
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2025-12-19 15:25:21 +0530

    Fix time complexity notation in Shell Sort comment

---
+++ Sequences/Sorting/Shell Sort.cpp
@@ -0,0 +1,28 @@
+// Time Complexity: O(n^2) Worst | O(n(logn)^2) Average | O(nlogn) Best 
+// Memory Complexity: O(1)
+// Stable: Yes
+// Mechanism to sort sequences by performing sort in an exponentially decreasing SHELL gap size
+
+
+void shell_sort(ll a[], ll n)
+{
+    for (ll gap = n / 2; gap > 0; gap /= 2)
+    {
+        for (ll i = gap; i < n; i++)
+        {
+            ll curr = a[i];
+            ll j = i - gap;
+            while (j >= 0 && a[j] > curr)
+            {
+                a[j + gap] = a[j];
+                j -= gap;
+            }
+            a[j + gap] = curr;
+        }
+    }
+    return;
+}
+
+
+---------------------------------------
+Note: The time complexity is entirely dependent on the shell gap. The worst case time complexity is for the above documented original shell algorithm (n/2,n/4,n/8....1). There are better gap configurations that help us achieve better complexity but it’s out of scope.
