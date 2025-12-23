99c6c7a4426cb2f71bfc7b516b798577a48a757f
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2025-12-23 15:41:00 +0530

    Update Reverse Upper Bound.cpp

--- Sequences/Searching/Reverse Upper Bound.cpp
+++ Sequences/Searching/Reverse Upper Bound.cpp
@@ -1,39 +1,32 @@
 // Time Complexity: O(log n)
 // Memory Complexity: Recursive: O(log n)) | Iterative: O(1)
-// Identify the first < give element in a sorted sequence by discarding half of the search space in each step
+// Identify the first < given element in a sorted sequence by discarding half of the search space in each step
 
 
 // Recursive
-ll lower_than_recurse(ll a[], ll n, ll target, ll left, ll right)
+ll lower_than_recurse(ll a[], ll n, ll target, ll &result, ll left, ll right)
 {
     if (left > right)
     {
-        return -1;
-    }
-    if (left == right)
-    {
-        if (left >= 0 && a[left] >= target)
-        {
-            left--;
-        }
-        return left;
+        return result;
     }
 
     ll mid = left + (right - left + 1) / 2;
 
     if (a[mid] < target)
     {
-        return lower_than_recurse(a, n, target, mid, right);
+        return lower_than_recurse(a, n, target, mid, mid + 1, right);
     }
     else
     {
-        return lower_than_recurse(a, n, target, left, mid - 1);
+        return lower_than_recurse(a, n, target, result, left, mid - 1);
     }
 }
 
 ll lower_than(ll a[], ll n, ll target)
 {
-    return lower_than_recurse(a, n, target, 0, n - 1);
+    ll result = -1;
+    return lower_than_recurse(a, n, target, result, 0, n - 1);
 }
 
 -------------------------------
@@ -41,13 +34,15 @@ ll lower_than(ll a[], ll n, ll target)
 // Iterative
 ll lower_than(ll a[], ll n, ll target)
 {
+    ll result = -1;
     ll left = 0, right = n - 1;
-    while (left < right)
+    while (left <= right)
     {
         ll mid = left + (right - left + 1) / 2;
         if (a[mid] < target)
         {
-            left = mid;
+            result = mid;
+            left = mid + 1;
         }
         else
         {
@@ -55,9 +50,5 @@ ll lower_than(ll a[], ll n, ll target)
         }
     }
 
-    if (left >= 0 && a[left] >= target)
-    {
-        left--;
-    }
-    return left;
+    return result;
 }
