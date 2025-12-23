f4ea79d0e06d690aa9babe261e8d41008a46da8f
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2025-12-23 15:18:44 +0530

    Update First Search.cpp

--- Sequences/Searching/First Search.cpp
+++ Sequences/Searching/First Search.cpp
@@ -1,46 +1,44 @@
 // Time Complexity: O(log n)
 // Memory Complexity: Recursive: O(log n)) | Iterative: O(1)
-// Identify the first occurence of a given element in a sorted sequence by discarding half of the search space in each step
-
+// Identify the first occurence of a given element in a sorted sequence by discarding half of the search space in each
+// step
 
 // Recursive
-ll first_search_recurse(ll a[], ll target, ll left, ll right)
+ll first_search_recurse(ll a[], ll target, ll &result, ll left, ll right)
 {
     if (left > right)
     {
-        return -1;
-    }
-    if (left == right)
-    {
-        return ((a[left] == target) ? left : -1);
+        return result;
     }
 
     ll mid = left + (right - left) / 2;
 
     if (a[mid] < target)
     {
-        return first_search_recurse(a, target, mid + 1, right);
+        return first_search_recurse(a, target, result, mid + 1, right);
     }
     else if (a[mid] == target)
     {
-        return first_search_recurse(a, target, left, mid);
+        return first_search_recurse(a, target, mid, left, mid - 1);
     }
     else
     {
-        return first_search_recurse(a, target, left, mid - 1);
+        return first_search_recurse(a, target, result, left, mid - 1);
     }
 }
 
 ll first_search(ll a[], ll n, ll target)
 {
-    return first_search_recurse(a, target, 0, n - 1);
+    ll result = -1;
+    return first_search_recurse(a, target, result, 0, n - 1);
 }
 
 // Iterative
 ll first_search(ll a[], ll n, ll target)
 {
     ll left = 0, right = n - 1;
-    while (left < right)
+    ll result = -1;
+    while (left <= right)
     {
         ll mid = left + (right - left) / 2;
         if (a[mid] < target)
@@ -49,7 +47,8 @@ ll first_search(ll a[], ll n, ll target)
         }
         else if (a[mid] == target)
         {
-            right = mid;
+            result = mid;
+            right = mid - 1;
         }
         else
         {
@@ -57,5 +56,5 @@ ll first_search(ll a[], ll n, ll target)
         }
     }
 
-    return ((a[left] == target) ? left : -1);
+    return result;
 }
