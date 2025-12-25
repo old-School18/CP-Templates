91c508ddb9b43ce65a0d01bafbbf69bcb65c3b98
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2025-12-25 20:35:53 +0530

    Create Reverse LL.cpp

---
+++ Data Structures/Linked Lists/Reverse LL.cpp
@@ -0,0 +1,57 @@
+// Time Complexity: O(n)
+// Memory Complexity: Recursive: O(n) | Iterative: O(1)
+// Reverse a single Linked List
+
+// Recursive
+Node *reverse_LL_recurse(Node *prevNode, Node *curr)
+{
+    if (curr == nullptr)
+    {
+        return prevNode;
+    }
+
+    Node *nextNode = curr->next;
+    curr->next = prevNode;
+    return reverse_LL_recurse(curr, nextNode);
+}
+Node *reverse_LL(Node *head)
+{
+    return reverse_LL_recurse(nullptr, head);
+}
+
+
+-------------------------------
+
+// Recursive #2
+Node *reverse_LL_recursive(Node *head)
+{
+    if (head == nullptr || head->next == nullptr)
+    {
+        return head;
+    }
+
+    Node *prevNode = reverse_LL_recursive(head->next);
+
+    Node *nextNode = head->next;
+    nextNode->next = head;
+
+    return prevNode;
+}
+
+-------------------------------
+
+// Iterative
+Node *reverse_LL(Node *head)
+{
+    Node *prevNode = nullptr;
+    Node *curr = head;
+    while (curr != nullptr)
+    {
+        Node *nextNode = curr->next;
+        curr->next = prevNode;
+        prevNode = curr;
+        curr = nextNode;
+    }
+
+    return prevNode;
+}
