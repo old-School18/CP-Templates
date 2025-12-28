3aa65c155572a08789f9c236a17061161382f5ef
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2025-12-28 20:44:13 +0530

    To camelCase

--- Math/Algebra/Factorization.cpp
+++ Math/Algebra/Factorization.cpp
@@ -2,20 +2,20 @@
 // Memory Complexity => O(log n)
 // Prime factors of a number
 
-void factorization(ll n, vector<ll> &prime_factors)
+void factorization(ll n, vector<ll> &primeFactors)
 {
     for (ll i = 2; i * i <= n; i++)
     {
         while (n % i == 0)
         {
-            prime_factors.push_back(i);
+            primeFactors.push_back(i);
             n /= i;
         }
     }
 
     if (n > 1)
     {
-        prime_factors.push_back(n);
+        primeFactors.push_back(n);
     }
     return;
 }
