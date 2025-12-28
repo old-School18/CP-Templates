31ad37126d69ada59447c0cf0beb9ecbb6b749e4
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2025-12-28 20:53:19 +0530

    Rename negative_exponent to negativeExponent

--- Math/Algebra/Negative Binary Exponent.cpp
+++ Math/Algebra/Negative Binary Exponent.cpp
@@ -1,24 +1,24 @@
 // Time Complexity => O(log y)
 // Memory Complexity => Recursive: O(log y) | Iterative: O(1) 
-// x^y ; works for all y
+// x^y — works for all y
 
 // Recursive
-ld negative_exponent(ld x, ll y)
+ld negativeExponent(ld x, ll y)
 {
     if (!y)
         return 1.0;
     else if (y < 0)
-        return negative_exponent(1.0 / x, -y);
+        return negativeExponent(1.0 / x, -y);
     else if (!(y % 2ll))
-        return negative_exponent(x * x, y / 2ll);
+        return negativeExponent(x * x, y / 2ll);
     else
-        return (x * negative_exponent(x * x, y / 2ll));
+        return (x * negativeExponent(x * x, y / 2ll));
 }
 
 ------------------------------------
 
 // Iterative
-ld negative_exponent(ld x, ll y)
+ld negativeExponent(ld x, ll y)
 {
     ld result = 1.0;
     if (y < 0)
