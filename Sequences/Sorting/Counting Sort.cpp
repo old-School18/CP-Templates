858336781082ac67082831e5ad5b7c6f4f0a3f83
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2025-12-29 01:34:11 +0530

    Update Counting Sort.cpp

--- Sequences/Sorting/Counting Sort.cpp
+++ Sequences/Sorting/Counting Sort.cpp
@@ -3,21 +3,21 @@
 // Stable: Yes
 // Mechanism to sort sequences by COUNTING number of elements less than equal to each element in the sequence
 
-void counting_sort(ll a[], ll n)
+void countingSort(ll a[], ll n)
 {
-    ll max_elem = a[0];
+    ll maxElem = a[0];
     for (ll i = 0; i < n; i++)
     {
-        max_elem = max(max_elem, a[i]);
+        maxElem = max(maxElem, a[i]);
     }
 
-    ll count[max_elem + 1] = {0};
+    ll count[maxElem + 1] = {0};
 
     for (ll i = 0; i < n; i++)
     {
         count[a[i]]++;
     }
-    for (ll i = 1; i <= max_elem; i++)
+    for (ll i = 1; i <= maxElem; i++)
     {
         count[i] += count[i - 1];
     }
