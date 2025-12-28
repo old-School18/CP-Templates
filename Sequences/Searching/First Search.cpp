3f3d058e81a0dfb0abd65ac4d9026b1d944cd369
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2025-12-29 01:26:11 +0530

    Rename search functions for consistency

--- Sequences/Searching/First Search.cpp
+++ Sequences/Searching/First Search.cpp
@@ -1,10 +1,10 @@
 // Time Complexity: O(log n)
 // Memory Complexity: Recursive: O(log n)) | Iterative: O(1)
-// Identify the first occurence of a given element in a sorted sequence by discarding half of the search space in each
+// Identify the first occurrence of a given element in a sorted sequence by discarding half of the search space in each
 // step
 
 // Recursive
-ll first_search_recurse(ll a[], ll target, ll &result, ll left, ll right)
+ll firstSearchRecurse(ll a[], ll target, ll &result, ll left, ll right)
 {
     if (left > right)
     {
@@ -15,28 +15,28 @@ ll first_search_recurse(ll a[], ll target, ll &result, ll left, ll right)
 
     if (a[mid] < target)
     {
-        return first_search_recurse(a, target, result, mid + 1, right);
+        return firstSearchRecurse(a, target, result, mid + 1, right);
     }
     else if (a[mid] == target)
     {
-        return first_search_recurse(a, target, mid, left, mid - 1);
+        return firstSearchRecurse(a, target, mid, left, mid - 1);
     }
     else
     {
-        return first_search_recurse(a, target, result, left, mid - 1);
+        return firstSearchRecurse(a, target, result, left, mid - 1);
     }
 }
 
-ll first_search(ll a[], ll n, ll target)
+ll firstSearch(ll a[], ll n, ll target)
 {
     ll result = -1;
-    return first_search_recurse(a, target, result, 0, n - 1);
+    return firstSearchRecurse(a, target, result, 0, n - 1);
 }
 
 -----------------------------
     
 // Iterative
-ll first_search(ll a[], ll n, ll target)
+ll firstSearch(ll a[], ll n, ll target)
 {
     ll result = -1;
     ll left = 0, right = n - 1;
