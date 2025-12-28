2c633174a4fea4b6ab686a8bd02acbb9cb2e7f7a
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2025-12-29 01:24:28 +0530

    Update Median of 2 Sorted.cpp

--- Sequences/Order Statistics/Median of 2 Sorted.cpp
+++ Sequences/Order Statistics/Median of 2 Sorted.cpp
@@ -4,93 +4,93 @@
 
 
 // Recursive
-ld median_of_two_recurse(ll a[], ll b[], ll n, ll m, ll k, ll left, ll right)
+ld medianOfTwoRecurse(ll a[], ll b[], ll n, ll m, ll k, ll left, ll right)
 {
     if (left > right)
     {
         return -1.0;
     }
 
-    ll partition_a = left + (right - left) / 2;
-    ll partition_b = k - partition_a;
+    ll partitionA = left + (right - left) / 2;
+    ll partitionB = k - partitionA;
 
-    ll max_left_a = (!partition_a) ? LLONG_MIN : a[partition_a - 1];
-    ll min_right_a = (partition_a == n) ? LLONG_MAX : a[partition_a];
-    ll max_left_b = (!partition_b) ? LLONG_MIN : a[partition_b - 1];
-    ll min_right_b = (partition_b == m) ? LLONG_MAX : b[partition_b];
+    ll maxLeftA = (!partitionA) ? LLONG_MIN : a[partitionA - 1];
+    ll minRightA = (partitionA == n) ? LLONG_MAX : a[partitionA];
+    ll maxLeftB = (!partitionB) ? LLONG_MIN : a[partitionB - 1];
+    ll minRightB = (partitionB == m) ? LLONG_MAX : b[partitionB];
 
-    if (max_left_a <= min_right_b && max_left_b <= min_right_a)
+    if (maxLeftA <= minRightB && maxLeftB <= minRightA)
     {
         if ((n + m) % 2)
         {
-            return max(max_left_a, max_left_b);
+            return max(maxLeftA, maxLeftB);
         }
         else
         {
-            return (max(max_left_a, max_left_b) + min(min_right_a, min_right_b)) / 2.0;
+            return (max(maxLeftA, maxLeftB) + min(minRightA, minRightB)) / 2.0;
         }
     }
-    else if (max_left_a > min_right_b)
+    else if (maxLeftA > minRightB)
     {
-        return median_of_two_recurse(a, b, n, m, k, left, partition_a - 1);
+        return medianOfTwoRecurse(a, b, n, m, k, left, partitionA - 1);
     }
     else
     {
-        return median_of_two_recurse(a, b, n, m, k, partition_a + 1, right);
+        return medianOfTwoRecurse(a, b, n, m, k, partitionA + 1, right);
     }
 }
 
-ld median_of_two(ll a[], ll b[], ll n, ll m)
+ld medianOfTwo(ll a[], ll b[], ll n, ll m)
 {
     if (n > m)
     {
-        return median_of_two(b, a, m, n);
+        return medianOfTwo(b, a, m, n);
     }
     ll k = (n + m + 1) / 2;
 
-    return median_of_two_recurse(a, b, n, m, k, max(0ll, k - m), min(k, n));
+    return medianOfTwoRecurse(a, b, n, m, k, max(0ll, k - m), min(k, n));
 }
 
 -------------------------------
 
 // Iterative
-ld median_of_two(ll a[], ll b[], ll n, ll m)
+ld medianOfTwo(ll a[], ll b[], ll n, ll m)
 {
     if (n > m)
     {
-        return median_of_two(b, a, m, n);
+        return medianOfTwo(b, a, m, n);
     }
     ll k = (n + m + 1) / 2;
 
     ll left = max(0ll, k - m), right = min(k, n);
     while (left <= right)
     {
-        ll partition_a = left + (right - left) / 2;
-        ll partition_b = k - partition_a;
+        ll partitionA = left + (right - left) / 2;
+        ll partitionB = k - partitionB;
 
-        ll max_left_a = (!partition_a) ? LLONG_MIN : a[partition_a - 1];
-        ll min_right_a = (partition_a == n) ? LLONG_MAX : a[partition_a];
-        ll max_left_b = (!partition_b) ? LLONG_MIN : b[partition_b - 1];
-        ll min_right_b = (partition_b == m) ? LLONG_MAX : b[partition_b];
+        ll maxLeftA = (!partitionA) ? LLONG_MIN : a[partitionA - 1];
+        ll minRightA = (partitionA == n) ? LLONG_MAX : a[partitionA];
+        ll maxLeftB = (!partitionB) ? LLONG_MIN : b[partitionB - 1];
+        ll minRightB = (partitionB == m) ? LLONG_MAX : b[partitionB];
 
-        if (max_left_a <= min_right_b && max_left_b <= min_right_a)
+        if (maxLeftA <= minRightB && maxLeftB <= minRightA)
         {
             if ((n + m) % 2)
             {
-                return max(max_left_a, max_left_b);
+                return max(maxLeftA, maxLeftB);
             }
             else
             {
-                return (max(max_left_a, max_left_b) + min(min_right_a, min_right_b)) / 2.0;
+                return (max(maxLeftA, maxLeftB) + min(minRightA, minRightB)) / 2.0;
             }
         }
-        else if (max_left_a > min_right_b)
+        else if (maxLeftA > minRightB)
         {
-            right = partition_a - 1;
+            right = partitionA - 1;
         }
         else
         {
-            left = partition_a + 1;
+            left = partitionA + 1;
         }
     }
 
