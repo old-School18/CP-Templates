073479ae8365bccf533580ccaf207fce8b732569
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2025-12-28 20:50:59 +0530

    Refactor function names for consistency

--- Math/Algebra/Modular Inverse.cpp
+++ Math/Algebra/Modular Inverse.cpp
@@ -1,25 +1,25 @@
 // Time Complexity => O(log(min(a,M))
-// Memory Complexity => Recursive: O(log min(a,M)) | Iterative: O(1)  [Extended Eulid]
+// Memory Complexity => Recursive: O(log min(a,M)) | Iterative: O(1)  [Extended Euclid]
 // Determines 'inv' where (a * inv) % M = 1
 
-ll extended_euclid(ll a, ll b, ll &x, ll &y)
+ll extendedEuclid(ll a, ll b, ll &x, ll &y)
 {
     if (b == 0)
     {
         x = 1, y = 0;
         return a;
     }
-    ll g = extended_euclid(b, a % b, x, y);
+    ll g = extendedEuclid(b, a % b, x, y);
     tie(x, y) = make_tuple(y, x - (a / b) * y);
     return g;
 }
 
-ll mod_inverse(ll a, ll M)
+ll modInverse(ll a, ll M)
 {
     ll x, y;
-    ll g = extended_euclid(a, M, x, y);
+    ll g = extendedEuclid(a, M, x, y);
     if (g != 1) {
     	return -1; // No inverse
     }
-    return x %  M;
+    return x % M;
 }
