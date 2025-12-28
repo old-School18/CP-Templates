cc25503acf8c2dfd797f4b48ea03dc947f41d2d5
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2025-12-28 20:57:53 +0530

    Refactor Sieve of Eratosthenes function naming
    Renamed function to follow camelCase convention and updated variable names for consistency. Adjusted comments for clarity.

--- Math/Algebra/Sieve of Eratosthenes.cpp
+++ Math/Algebra/Sieve of Eratosthenes.cpp
@@ -2,17 +2,17 @@
 // Memory Complexity => O(n)
 // Determine prime numbers from 1 to n.
 
-void sieve_of_eratosthenes(ll n)
+void sieveOfEratosthenes(ll n)
 {
-    vector<char> is_prime(n + 1, 1);
-    is_prime[0] = is_prime[1] = 0;
+    vector<char> isPrime(n + 1, 1);
+    isPrime[0] = isPrime[1] = 0;
     for (ll i = 2; i * i <= n; i++)
     {
-        if (is_prime[i])
+        if (isPrime[i])
         {
             for (ll j = i * i; j <= n; j += i)
             {
-                is_prime[j] = 0;
+                isPrime[j] = 0;
             }
         }
     }
@@ -21,4 +21,6 @@ void sieve_of_eratosthenes(ll n)
 }
 
 -----------------------------------
-Note:  Use vector<char> for time efficiency and vector<bool> for memory efficiency. vector<bool> operates at bit level rather than bytes so 1 element will occupy only 1 bit instead of a byte that a typical boolean variables does. So, this is memory efficient
+Note:  
+Use vector<char> for time efficiency and vector<bool> for memory efficiency. 
+vector<bool> operates at bit level rather than bytes so 1 element will occupy only 1 bit instead of a byte that a typical boolean variables does. So, this is memory efficient
