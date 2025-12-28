b0eb2f56a01f5c6fc46705479e6023c1fd4899ad
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2025-12-29 01:26:49 +0530

    Rename search functions to camelCase

--- Sequences/Searching/Last Search.cpp
+++ Sequences/Searching/Last Search.cpp
@@ -1,10 +1,10 @@
 // Time Complexity: O(log n)
 // Memory Complexity: Recursive: O(log n)) | Iterative: O(1)
-// Identify the last occurence of a given element in a sorted sequence by discarding half of the search space in each
+// Identify the last occurrence of a given element in a sorted sequence by discarding half of the search space in each
 // step
 
 // Recursive
-ll last_search_recurse(ll a[], ll target, ll &result, ll left, ll right)
+ll lastSearchRecurse(ll a[], ll target, ll &result, ll left, ll right)
 {
     if (left > right)
     {
@@ -15,28 +15,28 @@ ll last_search_recurse(ll a[], ll target, ll &result, ll left, ll right)
 
     if (a[mid] < target)
     {
-        return last_search_recurse(a, target, result, mid + 1, right);
+        return lastSearchRecurse(a, target, result, mid + 1, right);
     }
     else if (a[mid] == target)
     {
-        return last_search_recurse(a, target, mid, mid + 1, right);
+        return lastSearchRecurse(a, target, mid, mid + 1, right);
     }
     else
     {
-        return last_search_recurse(a, target, result, left, mid - 1);
+        return lastSearchRecurse(a, target, result, left, mid - 1);
     }
 }
 
-ll last_search(ll a[], ll n, ll target)
+ll lastSearch(ll a[], ll n, ll target)
 {
     ll result = -1;
-    return last_search_recurse(a, target, result, 0, n - 1);
+    return lastSearchRecurse(a, target, result, 0, n - 1);
 }
 
 -------------------------
 
 // Iterative
-ll last_search(ll a[], ll n, ll target)
+ll lastSearch(ll a[], ll n, ll target)
 {
     ll result = -1;
     ll left = 0, right = n - 1;
