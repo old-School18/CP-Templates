a2011ab50e22c2598b0c7e705c1bb4f847d12442
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2025-12-29 13:44:06 +0530

    Update Palindrome.cpp

--- Data Structures/Linked Lists/Palindrome.cpp
+++ Data Structures/Linked Lists/Palindrome.cpp
@@ -1,47 +1,41 @@
 // Time Complexity: O(n)
-// Memory Complexity: O(1)
-// Determine whether a linked list is palindrome or not
+// Memory Complexity: Recursive: O(n) | Iterative: O(1)
+// Reverse a double Linked List
 
-Node *tortoise_hare(Node *head)
+// Recursive
+DoubleNode *reverseDLLRecurse(DoubleNode *prevNode, DoubleNode *curr)
 {
-    Node *slow = head, *fast = head;
-
-    while (fast != nullptr && fast->next != nullptr)
+    if (curr == nullptr)
     {
-        slow = slow->next;
-        fast = fast->next->next;
+        return prevNode;
     }
 
-    return slow;
+    DoubleNode *nextNode = curr->next;
+    curr->prev = nextNode;
+    curr->next = prevNode;
+    return reverseDLLRecurse(curr, nextNode);
 }
 
-Node *reverse_LL(Node *head)
+DoubleNode *reverseDLL(DoubleNode *head)
 {
-    Node *prevNode = nullptr;
-    Node *curr = head;
+    return reverseDLLRecurse(nullptr, head);
+}
+
+-------------------------------
+
+// Iterative
+DoubleNode *reverseDLL(DoubleNode *head)
+{
+    DoubleNode *prevNode = nullptr;
+    DoubleNode *curr = head;
     while (curr != nullptr)
     {
-        Node *nextNode = curr->next;
+        DoubleNode *nextNode = curr->next;
+        curr->prev = nextNode;
         curr->next = prevNode;
         prevNode = curr;
         curr = nextNode;
     }
 
     return prevNode;
-}
-bool is_palindrome(Node *head)
-{
-    Node *middle_node = tortoise_hare(head);
-    Node *tail = reverse_LL(middle_node);
-
-    while (head->data == tail->val)
-    {
-        if (tail == middle_node)
-        {
-            return true;
-        }
-        head = head->next;
-        tail = tail->next;
-    }
-    return false;
 }
