8952fad7fcbe8b0296e7ae2bf86a5380ffa9e31c
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2025-12-29 13:46:44 +0530

    Update Tortoise Hare.cpp

--- Data Structures/Linked Lists/Tortoise Hare.cpp
+++ Data Structures/Linked Lists/Tortoise Hare.cpp
@@ -3,24 +3,24 @@
 // Identify the middle element of a linked list. For even sized lists, identify the latter middle element. If the requirement is to identify the former middle element in even sized lists, initialise fast as head->next
 
 // Recursive
-Node *tortoise_hare_recurse(Node *slow, Node *fast)
+Node *tortoiseHareRecurse(Node *slow, Node *fast)
 {
     if (fast == nullptr || fast->next == nullptr)
     {
         return slow;
     }
 
-    return tortoise_hare_recurse(slow->next, fast->next->next);
+    return tortoiseHareRecurse(slow->next, fast->next->next);
 }
-Node *tortoise_hare(Node *head)
+Node *tortoiseHare(Node *head)
 {
-    return tortoise_hare_recurse(head, head);
+    return tortoiseHareRecurse(head, head);
 }
 
 -------------------------------
 
 // Iterative
-Node *tortoise_hare(Node *head)
+Node *tortoiseHare(Node *head)
 {
     Node *slow = head, *fast = head;
 
