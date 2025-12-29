f243b6c3466c10de0cd5d94e9f813b86253f4ba0
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2025-12-29 17:40:43 +0530

    Update Combination Sum I.cpp

--- Recursion & Backtracking/Combination Sum I.cpp
+++ Recursion & Backtracking/Combination Sum I.cpp
@@ -3,7 +3,7 @@
 // Given an array of DISTINCT integers, determine all the unique combinations of the integers that sum up to a given value. An integer can be used multiple times.
 
 // Inclusion-Exclusion strategy
-void subset_sum_backtrack(vector<ll> &candidates, ll target, vector<vector<ll>> &results, vector<ll> &temp, ll idx)
+void combination_sum_backtrack(vector<ll> &candidates, ll target, vector<vector<ll>> &results, vector<ll> &temp, ll idx)
 {
     if (target < 0 || idx == (ll) candidates.size())
     {
@@ -16,17 +16,17 @@ void subset_sum_backtrack(vector<ll> &candidates, ll target, vector<vector<ll>> 
     }
 
     temp.push_back(candidates[idx]);
-    subset_sum_backtrack(candidates, target - candidates[idx], results, temp, idx);
+    combination_sum_backtrack(candidates, target - candidates[idx], results, temp, idx);
     temp.pop_back();
 
-    subset_sum_backtrack(candidates, target, results, temp, idx + 1);
+    combination_sum_backtrack(candidates, target, results, temp, idx + 1);
     return;
 }
 
-vector<vector<ll>> subset_sum(vector<ll> &candidates, ll target)
+vector<vector<ll>> combination_sum(vector<ll> &candidates, ll target)
 {
     vector<vector<ll>> results;
     vector<ll> temp;
-    subset_sum_backtrack(candidates, target, results, temp, 0);
+    combination_sum_backtrack(candidates, target, results, temp, 0);
     return results;
 }
