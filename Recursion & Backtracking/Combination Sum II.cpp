463ebd8a8bcff8e781af3f2043f2f70b7ce1ff86
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2025-12-29 19:53:26 +0530

    Update Combination Sum II.cpp

--- Recursion & Backtracking/Combination Sum II.cpp
+++ Recursion & Backtracking/Combination Sum II.cpp
@@ -1,16 +1,16 @@
-// Time Complexity => O(2ⁿ.n) where T - Target and M - Minimum candidate value 
+// Time Complexity => O(2^n.n)
 // Memory Complexity => O(n)
 // Given an array of NON-DISTINCT integers, determine all the unique combinations of the integers that sum up to a given value. An integer can be used only once.
 
 // Iterative strategy
-void combination_sum2_backtrack(vector<ll> &candidates, ll target, vector<vector<ll>> &results, vector<ll> &temp, ll idx)
+void combinationSum2Backtrack(vector<ll> &candidates, ll target, vector<vector<ll>> &results, vector<ll> &temp, ll idx)
 {
     if (target == 0)
     {
         results.push_back(temp);
         return;
     }
-    else if (target < 0 || idx == (ll) candidates.size())
+    else if (target < 0)
     {
         return;
     }
@@ -20,25 +20,27 @@ void combination_sum2_backtrack(vector<ll> &candidates, ll target, vector<vector
         if (i > idx && candidates[i] == candidates[i - 1])
             continue;
         temp.push_back(candidates[i]);
-        combination_sum2_backtrack(candidates, target - candidates[i], results, temp, i + 1);
+        combinationSum2Backtrack(candidates, target - candidates[i], results, temp, i + 1);
         temp.pop_back();
     }
+
+     return;
 }
 
-vector<vector<ll>> combination_sum2(vector<ll> &candidates, ll target)
+vector<vector<ll>> combinationSum2(vector<ll> &candidates, ll target)
 {
     vector<vector<ll>> results;
     vector<ll> temp;
     sort(candidates.begin(), candidates.end());
-    combination_sum2_backtrack(candidates, target, results, temp, 0);
+    combinationSum2Backtrack(candidates, target, results, temp, 0);
     return results;
 }
 
 ------------------------------
 
 // Inclusion-Exclusion strategy
-void combination_sum2_backtrack(vector<ll> &candidates, ll target, vector<vector<ll>> &results, vector<ll> &temp,
-                                bool previous_selected, ll idx)
+void combinationSum2Backtrack(vector<ll> &candidates, ll target, vector<vector<ll>> &results, vector<ll> &temp,
+                                bool previousSelected, ll idx)
 {
     if (target == 0)
     {
@@ -50,21 +52,21 @@ void combination_sum2_backtrack(vector<ll> &candidates, ll target, vector<vector
         return;
     }
 
-    if (idx == 0 || candidates[idx] != candidates[idx - 1] || previous_selected)
+    if (idx == 0 || candidates[idx] != candidates[idx - 1] || previousSelected)
     {
         temp.push_back(candidates[idx]);
-        combination_sum2_backtrack(candidates, target - candidates[idx], results, temp, true, idx + 1);
+        combinationSum2Backtrack(candidates, target - candidates[idx], results, temp, true, idx + 1);
         temp.pop_back();
     }
-    combination_sum2_backtrack(candidates, target, results, temp, false, idx + 1);
+    combinationSum2Backtrack(candidates, target, results, temp, false, idx + 1);
     return;
 }
-vector<vector<ll>> combination_sum2(vector<ll> &candidates, ll target)
+vector<vector<ll>> combinationSum2(vector<ll> &candidates, ll target)
 {
     vector<vector<ll>> results;
     vector<ll> temp;
     sort(candidates.begin(), candidates.end());
-    combination_sum2_backtrack(candidates, target, results, temp, false, 0);
+    combinationSum2Backtrack(candidates, target, results, temp, false, 0);
     return results;
 }
 
