cc694fcfe294b46363dec5b11d7a53578a824c01
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2025-12-29 17:42:02 +0530

    Update Combination Sum I.cpp

--- Recursion & Backtracking/Combination Sum I.cpp
+++ Recursion & Backtracking/Combination Sum I.cpp
@@ -5,13 +5,13 @@
 // Inclusion-Exclusion strategy
 void combination_sum_backtrack(vector<ll> &candidates, ll target, vector<vector<ll>> &results, vector<ll> &temp, ll idx)
 {
-    if (target < 0 || idx == (ll) candidates.size())
-    {
-        return;
-    }
-    else if (target == 0)
+    if (target == 0)
     {
         results.push_back(temp);
+        return;
+    }
+    else if (target < 0 || idx == (ll) candidates.size())
+    {
         return;
     }
 
