db9b8063e4884f299be4f11b6a6dca52efbfa971
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2025-12-30 19:07:33 +0530

    Update Decimal To Binary.cpp

--- Bits/Decimal To Binary.cpp
+++ Bits/Decimal To Binary.cpp
@@ -8,8 +8,8 @@ vector<ll> decimalToBinary(ll n)
 
     while (n)
     {
-        binary.push_back(n % 2);
-        n /= 2;
+        binary.push_back(n & 1ll);
+        n >>= 2ll;
     }
 
     reverse(binary.begin(), binary.end());
