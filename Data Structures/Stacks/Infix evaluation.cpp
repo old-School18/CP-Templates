08872b788ef3e9defb9654f641d0731b595d9c90
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2026-01-02 16:54:21 +0530

    Create Infix evaluation.cpp

---
+++ Data Structures/Stacks/Infix evaluation.cpp
@@ -0,0 +1,121 @@
+// Time Complexity => O(n)
+// Memory Complexity => O(n)
+// Evaluate infix mathematical expressions
+
+
+ll getPrecedence(char opr)
+{
+    if (opr == '^')
+    {
+        return 3;
+    }
+    else if (opr == '/' || opr == '*' || opr == '%')
+    {
+        return 2;
+    }
+    else if (opr == '+' || opr == '-')
+    {
+        return 1;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+bool isRightAssociative(char opr)
+{
+    return opr == '^';
+}
+
+ll getNumber(string &expression, ll n, ll &k)
+{
+    ll value = 0;
+    while (k < n && isdigit(expression[k]))
+    {
+        value = value * 10 + (expression[k++] - '0');
+    }
+    return value;
+}
+
+ld applyOperation(ld x, ld y, char op)
+{
+    switch (op)
+    {
+        case '^':
+            return pow(x, y);
+        case '/':
+            return x / y;
+        case '*':
+            return x * y;
+        case '%':
+            return fmod(x, y);
+        case '+':
+            return x + y;
+        case '-':
+            return x - y;
+    }
+
+    return 0.0;
+}
+ld evaluateLogic(stack<ld> &operands, stack<char> &operators)
+{
+    ld value2 = operands.top();
+    operands.pop();
+    ld value1 = operands.top();
+    operands.pop();
+    char opr = operators.top();
+    operators.pop();
+
+    return applyOperation(value1, value2, opr);
+}
+ld evaluateInfix(string expression)
+{
+    ll n = expression.size();
+    stack<ld> operands;
+    stack<char> operators;
+
+    for (ll i = 0; i < n; i++)
+    {
+        if (isdigit(expression[i]))
+        {
+            ld value = 1.0 * getNumber(expression, n, i);
+            operands.push(value);
+            i--;
+        }
+        else if (expression[i] == '(')
+        {
+            operators.push(expression[i]);
+        }
+        else if (expression[i] == ')')
+        {
+            while (!operators.empty() && operators.top() != '(')
+            {
+                ld value = evaluateLogic(operands, operators);
+                operands.push(value);
+            }
+            operators.pop();
+        }
+        else
+        {
+            while (!operators.empty() && operators.top() != '(' &&
+                   ((getPrecedence(operators.top()) > getPrecedence(expression[i])) ||
+                    (getPrecedence(operators.top()) == getPrecedence(expression[i]) &&
+                     !isRightAssociative(expression[i]))))
+            {
+                ld value = evaluateLogic(operands, operators);
+                operands.push(value);
+            }
+
+            operators.push(expression[i]);
+        }
+    }
+
+    while (!operators.empty())
+    {
+        ld value = evaluateLogic(operands, operators);
+        operands.push(value);
+    }
+
+    return operands.top();
+}
