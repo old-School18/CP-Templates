f698707d6c7301d7da076ab9fabb4e2b7bf14ea7
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2026-01-03 21:48:37 +0530

    Update Infix to Postfix.cpp

--- Data Structures/Stacks/Infix to Postfix.cpp
+++ Data Structures/Stacks/Infix to Postfix.cpp
@@ -27,38 +27,16 @@ bool isRightAssociative(char opr)
     return opr == '^';
 }
 
-string getNumber(string &expression, ll n, ll &k)
-{
-    string value = "";
-    while (k < n && (isalpha(expression[k]) || isdigit(expression[k])))
-    {
-        value += expression[k++];
-    }
-    return value;
-}
-string evaluateLogic(stack<string> &operands, stack<char> &operators)
-{
-    string value2 = operands.top();
-    operands.pop();
-    string value1 = operands.top();
-    operands.pop();
-    char opr = operators.top();
-    operators.pop();
-
-    return value1 + " " + value2 + " " + opr;
-}
 string infixToPostfix(string &expression)
 {
     ll n = expression.size();
-    stack<string> operands;
+    string postfix = "";
     stack<char> operators;
     for (ll i = 0; i < n; i++)
     {
         if (isalpha(expression[i]) || isdigit(expression[i]))
         {
-            string value = getNumber(expression, n, i);
-            operands.push(value);
-            i--;
+            postfix += expression[i];
         }
         else if (expression[i] == '(')
         {
@@ -68,8 +46,8 @@ string infixToPostfix(string &expression)
         {
             while (!operators.empty() && operators.top() != '(')
             {
-                string value = evaluateLogic(operands, operators);
-                operands.push(value);
+                postfix += operators.top();
+                operators.pop();
             }
             operators.pop();
         }
@@ -80,8 +58,8 @@ string infixToPostfix(string &expression)
                     (getPrecedence(operators.top()) == getPrecedence(expression[i]) &&
                      !isRightAssociative(operators.top()))))
             {
-                string value = evaluateLogic(operands, operators);
-                operands.push(value);
+                postfix += operators.top();
+                operators.pop();
             }
             operators.push(expression[i]);
         }
@@ -89,9 +67,9 @@ string infixToPostfix(string &expression)
 
     while (!operators.empty())
     {
-        string value = evaluateLogic(operands, operators);
-        operands.push(value);
+        postfix += operators.top();
+        operators.pop();
     }
 
-    return operands.top();
+    return postfix;
 }
