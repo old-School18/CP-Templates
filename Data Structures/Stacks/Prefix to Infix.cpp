6ed90790288ed331dd29fad36b25b8f53d9be17d
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2026-01-04 01:50:24 +0530

    Create Prefix to Infix.cpp

---
+++ Data Structures/Stacks/Prefix to Infix.cpp
@@ -0,0 +1,100 @@
+// Time Complexity => O(n)
+// Memory Complexity => O(n)
+// Convert prefix to infix
+
+string getNumber(string &expression, ll n, ll &k)
+{
+    string value = "";
+    while (k < n && isdigit(expression[k]))
+    {
+        value += expression[k++];
+    }
+    return value;
+}
+
+string getNumberBackward(string &expression, ll &k)
+{
+    string value = "";
+    while (k >= 0 && isdigit(expression[k]))
+    {
+        value += expression[k--];
+    }
+
+    reverse(value.begin(), value.end());
+    return value;
+}
+
+string reverseMathExpression(string expression)
+{
+    string reversedExpression = "";
+    ll n = expression.size();
+    for (ll i = n - 1; i >= 0; i--)
+    {
+        if (isdigit(expression[i]))
+        {
+            string value = getNumberBackward(expression, i);
+            reversedExpression += value;
+            i++;
+        }
+        else if (isalpha(expression[i]))
+        {
+            reversedExpression += expression[i];
+        }
+        else if (expression[i] == '(')
+        {
+            reversedExpression += ')';
+        }
+        else if (expression[i] == ')')
+        {
+            reversedExpression += '(';
+        }
+        else
+        {
+            reversedExpression += expression[i];
+        }
+    }
+    return reversedExpression;
+}
+
+string deduceSubExpression(stack<string> &operands, char opr)
+{
+    string value2 = operands.top();
+    operands.pop();
+    string value1 = operands.top();
+    operands.pop();
+    return "(" + value1 + " " + opr + " " + value2 + ")";
+}
+
+string postfixToInfix(string &expression)
+{
+    ll n = expression.size();
+    stack<string> operands;
+    for (ll i = 0; i < n; i++)
+    {
+        if (isdigit(expression[i]))
+        {
+            string value = getNumber(expression, n, i);
+            operands.push(value);
+            i--;
+        }
+        else if (isalpha(expression[i]))
+        {
+            operands.push(string(1, expression[i]));
+        }
+        else if (expression[i] != ' ')
+        {
+            string value = deduceSubExpression(operands, expression[i]);
+            operands.push(value);
+        }
+    }
+
+    return operands.top();
+}
+
+string prefixToInfx(string &expression)
+{
+    expression = reverseMathExpression(expression);
+    string prefix = postfixToInfix(expression);
+    prefix = reverseMathExpression(prefix);
+    return prefix;
+}
