3018333d266a442a01fbbf083064ae4901fc20e5
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2026-01-15 03:56:54 +0530

    Update Longest K distinct.cpp

--- Sequences/Sliding Window & 2-Pointer/Longest K distinct.cpp
+++ Sequences/Sliding Window & 2-Pointer/Longest K distinct.cpp
@@ -25,6 +25,30 @@ ll longestKDistinct(string s, ll k)
     return ans;
 }
 
+-----------------------
+// Slow-fast pointer Instant
+ll longestKDistinct(string s, ll k)
+{
+    ll n = s.size();
+    ll tracker[256] = {0};
+    ll ans = 0, ck = 0;
+    for (ll slow = 0, fast = 0; fast < n; fast++)
+    {
+        tracker[s[fast]]++;
+        if (tracker[s[fast]] == 1)
+            ck++;
+
+        while (ck > k)
+        {
+            if (tracker[s[slow]] == 1)
+                ck--;
+            tracker[s[slow++]]--;
+        }
+        ans = max(ans, fast - slow + 1);
+    }
+    return ans;
+}
+
 
 -----------------------
 Reference:
