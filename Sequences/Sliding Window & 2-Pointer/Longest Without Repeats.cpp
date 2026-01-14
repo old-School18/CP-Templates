9a20144278270c9fd2908778dd21cb107af55d1c
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2026-01-15 02:26:17 +0530

    Create Longest Without Repeats.cpp

---
+++ Sequences/Sliding Window & 2-Pointer/Longest Without Repeats.cpp
@@ -0,0 +1,28 @@
+// Time Complexity => O(n)
+// Memory Complexity => O(256)
+// Longest subarray without repeating characters
+
+// Slow-fast pointer Longest
+ll longestWithoutRepeats(string s)
+{
+    ll n = s.size();
+    ll tracker[256];
+    for (ll i = 0; i < 256; i++)
+        tracker[i] = -1;
+    ll ans = 0;
+    for (ll slow = 0, fast = 0; fast < n; fast++)
+    {
+        if (tracker[s[fast]] != -1)
+        {
+            slow = max(slow, tracker[s[fast]] + 1);
+        }
+        tracker[s[fast]] = fast;
+        ans = max(ans, fast - slow + 1);
+    }
+    return ans;
+}
+
+
+--------------------------
+Reference:
+https://leetcode.com/problems/longest-substring-without-repeating-characters/
