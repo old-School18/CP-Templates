96f12f064662b161e4eeba6e20a43e50ee875684
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2026-01-15 02:49:53 +0530

    Create Longest K Distinct.cpp

---
+++ Sequences/Sliding Window & 2-Pointer/Longest K Distinct.cpp
@@ -0,0 +1,31 @@
+// Time Complexity => O(n)
+// Memory Complexity => O(256)
+// Longest subarray with atmost k distinct characters
+
+// Slow-fast pointer Lazy
+ll longestKDistinct(string s, ll k)
+{
+    ll n = s.size();
+    ll tracker[256] = {0};
+    ll ans = 0, ck = 0;
+    for (ll slow = 0, fast = 0; fast < n; fast++)
+    {
+        tracker[s[fast]]++;
+        if (tracker[s[fast]] == 1)
+            ck++;
+
+        if (ck > k)
+        {
+            if (tracker[s[slow]] == 1)
+                ck--;
+            tracker[s[slow++]]--;
+        }
+        ans = max(ans, fast - slow + 1);
+    }
+    return ans;
+}
+
+
+-----------------------
+Reference:
+https://takeuforward.org/data-structure/longest-substring-with-at-most-k-distinct-characters
