e8975749d36e6627f08f3f4c144a5e8b5156e3ab
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2026-01-15 03:48:02 +0530

    Update Binary subarray sum.cpp

--- Sequences/Sliding Window & 2-Pointer/Binary subarray sum.cpp
+++ Sequences/Sliding Window & 2-Pointer/Binary subarray sum.cpp
@@ -1,5 +1,5 @@
 // Time Complexity => O(n)
-// Memory Complexity => O(256)
+// Memory Complexity => O(1)
 // Number of subarrays of a binary array whose sum is k
 
 // Slow-fast pointer Instant
@@ -22,3 +22,34 @@ ll binarySubArraySum(ll a[], ll n, ll k)
     }
     return ans;
 }
+
+--------------------------
+// Slow-fast pointer Atmost
+ll twoPointerAtmost(ll a[], ll n, ll k)
+{
+    ll ans = 0, ck = 0;
+    for (ll slow = 0, fast = 0; fast < n; fast++)
+    {
+        if (a[fast])
+            ck++;
+        while (ck > k)
+        {
+            ck -= a[slow++];
+        }
+        ans += fast - slow + 1;
+    }
+    return ans;
+}
+
+ll binarySubArraySum(ll a[], ll n, ll k)
+{
+    if (!k)
+        return twoPointerAtmost(a, n, k);
+    return twoPointerAtmost(a, n, k) - twoPointerAtmost(a, n, k - 1);
+}
+
+
+
+--------------------------
+Reference:
+https://leetcode.com/problems/binary-subarrays-with-sum/
