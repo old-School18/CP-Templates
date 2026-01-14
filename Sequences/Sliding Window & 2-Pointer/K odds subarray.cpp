3d35172eda0635c991cfd1d384d68c0ad7f14610
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2026-01-15 04:02:20 +0530

    Create K odds subarray.cpp

---
+++ Sequences/Sliding Window & 2-Pointer/K odds subarray.cpp
@@ -0,0 +1,54 @@
+// Time Complexity => O(n)
+// Memory Complexity => O(256)
+// Number of subarrays of a binary array whose sum is k
+
+// Slow-fast pointer Instant
+ll kOddsSubarray(ll a[], ll n, ll k)
+{
+    ll ans = 0, ck = 0, prefixTracker = 0;
+    for (ll slow = 0, fast = 0; fast < n; fast++)
+    {
+        if (a[fast] % 2)
+            ck++;
+        while (slow < fast && (!(a[slow] % 2) || ck > k))
+        {
+            if (a[slow++] % 2)
+                ck--, prefixTracker = 0;
+            else
+                prefixTracker++;
+        }
+        if (ck == k)
+            ans += prefixTracker + 1;
+    }
+    return ans;
+}
+
+-----------------------
+// Slow-fast pointer Atmost
+ll twoPointerAtmost(ll a[], ll n, ll k)
+{
+    ll ans = 0, ck = 0;
+    for (ll slow = 0, fast = 0; fast < n; fast++)
+    {
+        if (a[fast] % 2)
+            ck++;
+        while (ck > k)
+        {
+            ck -= a[slow++] % 2;
+        }
+        ans += fast - slow + 1;
+    }
+    return ans;
+}
+
+ll kOddsSubarray(ll a[], ll n, ll k)
+{
+    if (!k)
+        return twoPointerAtmost(a, n, k);
+    return twoPointerAtmost(a, n, k) - twoPointerAtmost(a, n, k - 1);
+}
+
+
+--------------------------
+Reference:
+https://leetcode.com/problems/count-number-of-nice-subarrays/
