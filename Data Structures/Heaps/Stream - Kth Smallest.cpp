3346d1f5548748f6bfa038ec1926ebf656f0de8f
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2026-01-18 00:23:12 +0530

    Create Stream - Kth Smallest.cpp

---
+++ Data Structures/Heaps/Stream - Kth Smallest.cpp
@@ -0,0 +1,29 @@
+// Time Complexity => O(nlogk)
+// Memory Complexity => O(k)
+// Kth Smallest Element in a stream
+
+class KthSmallest
+{
+  public:
+    priority_queue<ll> pq;
+    ll k;
+
+    KthSmallest(ll k)
+    {
+        this->k = k;
+    }
+
+    void add(ll val)
+    {
+        pq.push(val);
+        if (pq.size() > k)
+        {
+            pq.pop();
+        }
+    }
+
+    ll kthSmallest()
+    {
+        return pq.empty() ? -1 : pq.top();
+    }
+};
