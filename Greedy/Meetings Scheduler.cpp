54f3300f92ee4d7df5c1c573b3468059ef3e3b25
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2026-01-29 14:52:07 +0530

    Add meeting scheduler implementation
    Implemented a meeting scheduler that maximizes the number of meetings scheduled without overlap.

---
+++ Greedy/Meetings Scheduler.cpp
@@ -0,0 +1,36 @@
+// Time Complexity: O(n log n)
+// Space Complexity: O(n)
+// Schedule maximum meetings possible when only one meeting is possibe at a time
+
+class SortMeetings
+{
+  public:
+    bool operator()(const pair<pair<ll, ll>, ll> &x, const pair<pair<ll, ll>, ll> &y)
+    {
+        return x.first.second < y.first.second;
+    }
+};
+
+vector<ll> meetingScheduler(vector<ll> &start, vector<ll> &end)
+{
+    ll n = start.size();
+    vector<ll> scheduledOrder;
+    vector<pair<pair<ll, ll>, ll>> scheduler;
+
+    for (ll i = 0; i < n; i++)
+    {
+        scheduler.push_back({{start[i], end[i]}, i});
+    }
+
+    sort(scheduler.begin(), scheduler.end(), SortMeetings());
+
+    for (auto &p : scheduler)
+    {
+        if (scheduledOrder.empty() || end[scheduledOrder.back()] <= p.first.first)
+        {
+            scheduledOrder.push_back(p.second);
+        }
+    }
+
+    return scheduledOrder;
+}
