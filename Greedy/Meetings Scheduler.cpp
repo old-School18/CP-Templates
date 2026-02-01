1bcb951cbec58145e886eedefcb1e4b7ddd816cb
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2026-02-01 21:56:53 +0530

    Update Meetings Scheduler.cpp

--- Greedy/Meetings Scheduler.cpp
+++ Greedy/Meetings Scheduler.cpp
@@ -1,8 +1,8 @@
 // Time Complexity: O(n log n)
 // Space Complexity: O(n)
-// Schedule maximum meetings possible when only one meeting is possibe at a time
+// Schedule maximum meetings possible when only one meeting is possible at a time
 
-// Activity Selection approach
+// Activity Selection approach – Maximum non-overlapping identification
 class Comparator
 {
   public:
