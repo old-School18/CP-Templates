77cab15dae18959cee3a6936c9572f246e7818ad
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2026-02-07 10:29:35 +0530

    Create Height.cpp

---
+++ Data Structures/Trees/Binary Trees/Height.cpp
@@ -0,0 +1,44 @@
+// Time Complexity: O(n)
+// Memory Complexity: O(h)
+// Height of a binary tree
+
+// Recursive
+ll heightBTRecursive(BT *root)
+{
+    if (!root)
+    {
+        return 0;
+    }
+
+    return 1ll + max(heightBTRecursive(root->left), heightBTRecursive(root->right));
+}
+
+------------------------
+// Iterative
+ll heightBT(BT *root)
+{
+    ll height = 0;
+    stack<pair<BT *, ll>> stk;
+
+    if (root)
+    {
+        stk.push({root, 1});
+    }
+
+    while (!stk.empty())
+    {
+        auto p = stk.top();
+        stk.pop();
+        height = max(height, p.second);
+
+        if (p.first->right)
+        {
+            stk.push({p.first->right, p.second + 1});
+        }
+        if (p.first->left)
+        {
+            stk.push({p.first->left, p.second + 1});
+        }
+    }
+    return height;
+}
