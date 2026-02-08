638e2af2c8f5bd4e0b614acd6078adea6d104861
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2026-02-08 16:45:22 +0530

    Update K Distance.cpp

--- Data Structures/Trees/Binary Trees/K Distance.cpp
+++ Data Structures/Trees/Binary Trees/K Distance.cpp
@@ -1,7 +1,8 @@
 // Time Complexity: O(n)
-// Memory Complexity: O(h) where h = tree height
+// Memory Complexity: O(h) DFS | O(n) BFS where h = tree height
 // All nodes that are at a distance k from a given node
 
+// DFS
 void collectFromSubTree(BT *root, ll k, ll depth, vector<ll> &kNodes)
 {
     if (root == nullptr || depth > k)
@@ -10,7 +11,7 @@ void collectFromSubTree(BT *root, ll k, ll depth, vector<ll> &kNodes)
     }
     if (depth == k)
     {
-        kNodes.push_back(root->val);
+        kNodes.push_back(root->data);
         return;
     }
     collectFromSubTree(root->left, k, depth + 1, kNodes);
@@ -31,24 +32,25 @@ ll dfs(BT *root, BT *target, ll k, vector<ll> &kNodes)
     }
 
     ll left = dfs(root->left, target, k, kNodes);
-    ll right = dfs(root->right, target, k, kNodes);
-
-    if (left != -1)
+	if (left != -1)
     {
         if (left == k)
-            kNodes.push_back(root->val);
+            kNodes.push_back(root->data);
         else
             collectFromSubTree(root->right, k, left + 1, kNodes);
         return left + 1;
     }
+	
+    ll right = dfs(root->right, target, k, kNodes);
     if (right != -1)
     {
         if (right == k)
-            kNodes.push_back(root->val);
+            kNodes.push_back(root->data);
         else
             collectFromSubTree(root->left, k, right + 1, kNodes);
         return right + 1;
     }
+	
     return -1;
 }
 
@@ -57,4 +59,76 @@ vector<ll> kDistance(BT *root, BT *target, ll k)
     vector<ll> kNodes;
     dfs(root, target, k, kNodes);
     return kNodes;
+}
+
+-------------------
+// BFS - with back-edges
+vector<ll> kDistance(BT *root, BT *target, ll k)
+{
+    if (!root || !target)
+        return {};
+
+    unordered_map<BT *, BT *> parentMap;
+    unordered_set<BT *> visited;
+    queue<BT *> qe;
+
+    qe.push(root);
+    while (!qe.empty())
+    {
+        auto p = qe.front();
+        qe.pop();
+
+        if (p->left)
+        {
+            parentMap[p->left] = p;
+            qe.push(p->left);
+        }
+        if (p->right)
+        {
+            parentMap[p->right] = p;
+            qe.push(p->right);
+        }
+    }
+
+    ll level = 0;
+    qe.push(target);
+    visited.insert(target);
+
+    while (!qe.empty())
+    {
+        ll currLeveLSize = qe.size();
+        if (level++ == k)
+        {
+            vector<ll> res;
+            while (!qe.empty())
+            {
+                res.push_back(qe.front()->val);
+                qe.pop();
+            }
+            return res;
+        }
+
+        while (currLeveLSize--)
+        {
+            auto p = qe.front();
+            qe.pop();
+
+            if (parentMap.count(p) && !visited.count(parentMap[p]))
+            {
+                visited.insert(parentMap[p]);
+                qe.push(parentMap[p]);
+            }
+            if (p->left && !visited.count(p->left))
+            {
+                visited.insert(p->left);
+                qe.push(p->left);
+            }
+            if (p->right && !visited.count(p->right))
+            {
+                visited.insert(p->right);
+                qe.push(p->right);
+            }
+        }
+    }
+    return {};
 }
