bdffb4421d6aa25f298b030ee62d10083fdee3d4
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2026-02-11 10:20:25 +0530

    Create Lower Bound.cpp

---
+++ Data Structures/Trees/Binary Search Trees/Lower Bound.cpp
@@ -0,0 +1,40 @@
+// Time Complexity: O(n)
+// Memory Complexity: O(1) Iterative | O(h) Recursive where h = tree height
+// Lower bound
+
+BST *lowerBoundRecursive(BST *root, ll target)
+{
+    if (root == nullptr)
+    {
+        return root;
+    }
+
+    if (root->data >= target)
+    {
+        BST *smallerLB = lowerBound(root->left, target);
+        return smallerLB ? smallerLB : root;
+    }
+    else
+    {
+        return lowerBound(root->right, target);
+    }
+}
+
+BST *lowerBound(BST *root, ll target)
+{
+    BST *lb = nullptr;
+
+    while (root)
+    {
+        if (root - data >= target)
+        {
+            lb = root;
+            root = root->left;
+        }
+        else
+        {
+            root = root->right;
+        }
+    }
+    return lb;
+}
