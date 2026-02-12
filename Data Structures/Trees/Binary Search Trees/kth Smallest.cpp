0c6eeaf6ee9ec31c1f9a41d0035e4ba0f114063c
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2026-02-12 12:00:07 +0530

    Update kth Smallest.cpp

--- Data Structures/Trees/Binary Search Trees/kth Smallest.cpp
+++ Data Structures/Trees/Binary Search Trees/kth Smallest.cpp
@@ -1,5 +1,5 @@
-// Time Complexity: O(h) where h = tree height
-// Memory Complexity: O(h)
+// Time Complexity: O(n) 
+// Memory Complexity: O(1) Morris | O(h) otherwise where h = tree height
 // kth Smallest Node  (In order traversal)
 
 // Recursive
@@ -50,4 +50,48 @@ ll kthSmallest(BST *root, ll k)
         root = root->right;
     }
     return -1;
+}
+
+
+-----------------
+// Iterative - Morris
+ll kthSmallest(BST *root, ll k)
+{
+    ll idx = 0;
+    ll kth = -1;
+    while (root)
+    {
+        if (root->left == nullptr)
+        {
+            if (++idx == k)
+            {
+                kth = root->val;
+            }
+            root = root->right;
+        }
+        else
+        {
+            BST *prev = root->left;
+            while (prev->right && prev->right != root)
+            {
+                prev = prev->right;
+            }
+
+            if (prev->right == nullptr)
+            {
+                prev->right = root;
+                root = root->left;
+            }
+            else
+            {
+                prev->right = nullptr;
+                if (++idx == k)
+                {
+                    kth = root->val;
+                }
+                root = root->right;
+            }
+        }
+    }
+    return kth;
 }
