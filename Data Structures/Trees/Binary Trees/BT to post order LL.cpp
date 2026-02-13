f1eb8d2594db6f4785f82d1b8cb62a8d8b028652
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2026-02-13 16:16:01 +0530

    Update BT to post order LL.cpp

--- Data Structures/Trees/Binary Trees/BT to post order LL.cpp
+++ Data Structures/Trees/Binary Trees/BT to post order LL.cpp
@@ -34,7 +34,103 @@ BT *postOrderLL(BT *root)
 
 -----------------
 // Iterative
+BT *reverseLL(BT *head)
+{
+    BT *prevNode = nullptr;
+    BT *curr = head;
+    while (curr != nullptr)
+    {
+        BT *nextNode = curr->right;
+        curr->right = prevNode;
+        prevNode = curr;
+        curr = nextNode;
+    }
 
+    return prevNode;
+}
+
+BT *postOrderLL(BT *root)
+{
+    BT *head = nullptr, *predecessor = nullptr;
+    stack<BT *> stk;
+
+    if (root)
+    {
+        stk.push(root);
+    }
+
+    while (!stk.empty())
+    {
+        auto p = stk.top();
+        stk.pop();
+
+        BT *leftChild = p->left;
+		BT *rightChild = p->right;
+        if (!head)
+        {
+            head = p;
+        }
+        else
+        {
+            predecessor->right = p;
+        }
+        p->left = nullptr;
+
+        if (leftChild)
+        {
+            stk.push(leftChild);
+        }
+        if (rightChild)
+        {
+            stk.push(rightChild);
+        }
+    }
+
+    return reverseLL(head);
+}
 
 ------------------
 // Iterative - Morris
+BT *flipLLPointers(BT *head)
+{
+    BT *curr = head;
+    BT *prev = nullptr;
+    while (curr != nullptr)
+    {
+        prev = curr->left;
+        if (prev)
+        {
+            prev->right = curr;
+        }
+        else
+        {
+            return curr;
+        }
+        curr->left = nullptr;
+        curr = prev;
+    }
+    return prev;
+}
+
+BT *postOrderLL(BT *root)
+{
+    BT *head = root;
+    while (root)
+    {
+        if (root->right != nullptr)
+        {
+            BT *prev = root->right;
+            while (prev->left)
+            {
+                prev = prev->left;
+            }
+
+            prev->left = root->left;
+            root->left = root->right;
+            root->right = nullptr;
+        }
+        root = root->left;
+    }
+
+    return flipLLPointers(head);
+}
