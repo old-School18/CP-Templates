b59c632d8fc658362decff2d6272916ffe4993d1
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2026-02-21 14:09:08 +0530

    Update Cycle detection Directed graph.cpp

--- Data Structures/Graphs/Cycle detection Directed graph.cpp
+++ Data Structures/Graphs/Cycle detection Directed graph.cpp
@@ -2,28 +2,28 @@
 // Memory Complexity: O(V)
 // Cycle detection in Directed graphs
 
-bool dfs(vector<vector<ll>> &adj, vector<bool> &vis, vector<bool> &ancestral_stk, ll u)
+bool dfs(vector<vector<ll>> &adj, vector<bool> &vis, vector<bool> &ancestralStk, ll u)
 {
     vis[u] = true;
-    ancestral_stk[u] = true;
+    ancestralStk[u] = true;
     for (auto &v : adj[u])
     {
-        if (!vis[v] && dfs(adj, vis, ancestral_stk, v))
+        if (!vis[v] && dfs(adj, vis, ancestralStk, v))
             return true;
-        else if (ancestral_stk[v])
+        else if (ancestralStk[v])
             return true;
     }
-    ancestral_stk[u] = false;
+    ancestralStk[u] = false;
     return false;
 }
 
 bool cycleDirected(vector<vector<ll>> &adj, ll n)
 {
     vector<bool> vis(n + 1, false);
-    vector<bool> ancestral_stk(n + 1, false);
+    vector<bool> ancestralStk(n + 1, false);
     for (ll i = 1; i <= n; i++)
     {
-        if (!vis[i] && dfs(adj, vis, ancestral_stk, i))
+        if (!vis[i] && dfs(adj, vis, ancestralStk, i))
             return true;
     }
     return false;
